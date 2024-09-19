581be8dc0a78674913988fe81cf711400b5a2c76
wafiq <wafiqgithub775@gmail.com>
2024-09-20 02:30:58 +0600

    FEAT: add apollo client & provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ApolloProvider from "@/queries/apolloProvider"
+import { apolloClient } from "@/queries/apolloClient"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ApolloProvider client={apolloClient}>
+          {children}
+        </ApolloProvider>
       </body>
     </html>
   );
