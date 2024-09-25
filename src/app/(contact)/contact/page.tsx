371fdee70cf2cad159116d0d5d61927f83a948ad
wafiq <wafiqgithub775@gmail.com>
2024-09-26 05:06:14 +0600

    FEAT: add contact route, View component, custom contact layout

---
+++ src/app/(contact)/contact/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { ContactGroupView } from "@/components/contact/View";
+
+const ContactPage: React.FC = () => {
+    return (
+        <>
+            <ContactGroupView />
+        </>
+    );
+};
+
+export default ContactPage;
